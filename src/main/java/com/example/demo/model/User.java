cb390f0d54823e1634d16fcc5a16439a70f19cd7
educcarnn <educcarnn@gmail.com>
2024-01-15 00:03:32 -0300

    feat: model user

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -4,6 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table
+@Table(name ="users")
 public class User {
+    private String name;
+
+    private String cpf;
+
+    private String password;
+
+    private String type;
+
+    private Float balance;
 }
