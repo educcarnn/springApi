da3eff303de18f59b5b1326151588b5312ff49e9
educcarnn <educcarnn@gmail.com>
2024-01-15 16:34:51 -0300

    fix: interface transaction

--- src/main/java/com/example/demo/repository/TransactionRepository.java
+++ src/main/java/com/example/demo/repository/TransactionRepository.java
@@ -4,6 +4,6 @@ import com.example.demo.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public class TransactionRepository extends JpaRepository<Transaction, Long> {
-    
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
 }
