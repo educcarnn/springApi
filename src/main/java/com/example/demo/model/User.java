5eccf195a134555657345805803aaa9dbd94f234
educcarnn <educcarnn@gmail.com>
2024-01-15 00:15:30 -0300

    fix: columns and id model users

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,18 +1,29 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name ="users")
 public class User {
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
+    @Column(length=92, nullable = false)
+    private String name;
+    
+    @Column(length=11, nullable = false, unique = true)
     private String cpf;
 
+    @Column(length=62, nullable = false, unique = true)
+    private String email;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String password;
 
+    @Column(length = 6, nullable = false)
     private String type;
 
+    @Column(columnDefinition = "DECIMAL DEFAULT 0.0")
     private Float balance;
 }
