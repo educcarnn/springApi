ec9d8472c600ae62811306561e0b0a136852361c
educcarnn <educcarnn@gmail.com>
2024-01-23 15:59:17 -0300

    feat: new models in java user

--- src/main/java/br/com/eduardo/learningspring/service/UserService.java
+++ src/main/java/br/com/eduardo/learningspring/service/UserService.java
@@ -26,5 +26,27 @@ public class UserService {
     public List<User> readUsers() {
         return userRepository.findAll();
     }
-    
+
+    public User retrieveUser(final long id) throws Exception{
+        return userRepository.findById(id).orElseThrow(() -> new Exception("User not found"));
+
+    }
+
+    public User updateUser(final User userData, final long id) throws  Exception{
+        final User foundUser = userRepository.findById(id).orElseThrow(() -> new Exception("User not found"));
+
+        foundUser.setName(userData.getName());
+        foundUser.setCpf(userData.getCpf());
+        foundUser.setEmail(userData.getEmail());
+        foundUser.setPassword(userData.getPassword());
+        foundUser.setType(userData.getType());
+
+        return userRepository.save(foundUser);
+    }
+
+    public void deleteUser(final long id ) throws Exception{
+        final User foundUser = userRepository.findById(id).orElseThrow(() -> new Exception("User not found"));
+
+        userRepository.delete(foundUser);
+    }
 }
